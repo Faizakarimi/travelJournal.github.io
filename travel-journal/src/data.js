9d232f6c0fc0e9b6b68134e16584b7685387e71f
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-07-09 13:00:35 +0500

    Create data.js

---
+++ travel-journal/src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://maps.app.goo.gl/Pta1zgGfCot1qPbF7",
+        imageUrl: "https://unsplash.com/photos/city-skyline-during-night-time-vcO5nYFjBlE",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney, New South Wales, Australia. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th-century architecture"
+
+
+    },
+
+    {
+        title: "Bali",
+        location: "Indonesia",
+        googleMapsUrl: "https://maps.app.goo.gl/RPGhwUZ8S8kt5aiy8",
+        imageUrl: "https://unsplash.com/photos/the-sun-is-setting-over-the-ocean-with-waves-coming-in-hi-Mx_2Wprw",
+        description: "Bali is a province of Indonesia and the westernmost of the Lesser Sunda Islands. East of Java and west of Lombok, the province includes the island of Bali and a few smaller offshore islands, notably Nusa Penida, Nusa Lembongan, and Nusa Ceningan to the southeast"
+
+    },
+
+    {
+        title: "Taj Mahal",
+        location: "India",
+        googleMapsUrl: "https://maps.app.goo.gl/QVx92mHg4YdPEwAUA",
+        imageUrl: "https://unsplash.com/photos/taj-mahal-indian-symbol-and-famous-tourist-destination-india-travel-background-agra-india-WQ6WY27_uhQ",
+        description: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, Uttar Pradesh, India. It was commissioned in 1631 by the fifth Mughal emperor, Shah Jahan to house the tomb of his beloved wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself."
+
+
+    }
+]
